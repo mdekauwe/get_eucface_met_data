83000b385dd87906f1f4fffe9db84fc16196ddf5
mdekauwe <mdekauwe@gmail.com>
2018-08-06 13:07:16 +1000

    Added CO2 to check script

--- check_met_data.py
+++ check_met_data.py
@@ -15,10 +15,19 @@ __email__   = "mdekauwe@gmail.com"
 def main():
 
     fname = "EucFACE_met_amb.nc"
-    ds = xr.open_dataset(fname)
+    dsa = xr.open_dataset(fname)
+    fname = "EucFACE_met_ele.nc"
+    dse = xr.open_dataset(fname)
 
-    x = ds.SWdown[:,0,0].values
-    plt.plot(x)
+    vars_to_keep = ['CO2']
+    dfa = dsa[vars_to_keep].squeeze(dim=["x","y"], drop=True).to_dataframe()
+    dfe = dse[vars_to_keep].squeeze(dim=["x","y"], drop=True).to_dataframe()
+    co2_a = dfa.CO2.resample('D').mean()
+    co2_e = dfe.CO2.resample('D').mean()
+
+    plt.plot(co2_e)
+    plt.plot(co2_a)
+    #plt.plot(co2_e/co2_a)
     plt.show()
 
 if __name__ == "__main__":
